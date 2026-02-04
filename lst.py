8d7e4e2cc554d54c32e26a9b8f2cb6893bd92c00
aneela-gth <aneelabandi9951@gmail.com>
2026-02-04 13:03:38 +0530

    day1

---
+++ lst.py
@@ -0,0 +1,132 @@
+# # Filter even numbers from a list
+# lst=[1,2,3,4,5,6,8]
+# even=[]
+# for i in lst:
+#     if i%2==0:
+#         even.append(i)
+# print(even)
+
+# #  Find duplicate elements in a list
+# a=[1,2,1,3,1,2,4]
+# dupl=[]
+# for i in a:
+#     if a.count(i)> 1 and i not in dupl:
+#         dupl.append(i)
+# print(dupl)
+
+# # Remove duplicates from a list
+# list=[10,20,10,30,10]
+# c=[]
+# for i in list:
+#     if i not in c:
+#         c.append(i)
+# print(c)
+
+# # Check if a list is empty
+# s=[]
+# if not s:
+#     print("it is empty")
+# else:
+#     print("it is not empty")
+
+# #  Reverse a list
+# lst=[1,2,3]
+# # rev=[]
+# # for i in lst:
+# #     rev.insert(0,i)
+# # print(rev)
+# lst.reverse()
+# print(lst)
+
+# # Find largest number in a list
+# lst=[10,50,20,40]
+# l=lst[0]
+# for i in lst:
+#     if i>l:
+#         l=i
+# print(l)
+
+# # Student Marks
+# m=[34,56,79]
+# t=sum(m)
+# print(t)
+
+# # Sum of elements in a list (without sum())
+# lst = [10, 20, 30]
+# t=0
+# for i in lst:
+#     t=t+i
+# print(t)
+
+
+# Count elements in a list (without len()) 
+# lst = [1, 2, 3, 4, 5]
+# c=0
+# for i in lst:
+#     c+=1
+# print(c)
+
+# Find smallest number in a list (without min())
+
+# lst = [10, 5, 20, 3]
+# a=lst[0]
+# for i in lst:
+#     if i<a:
+#         a=i
+# print(a)
+
+
+#  Check if element exists in list Print list 
+
+# lst = [10, 20, 30, 40]
+# key = 30
+# found = False
+
+# for i in lst:
+#     if i == key:
+#         found = True
+#         break
+
+# if found:
+#     print("Element exists in the list")
+# else:
+#     print("Element does not exist in the list")
+
+# elements one by one Reverse a list (without built-in methods)
+# lst = ["A", "B", "C"]
+# for i in lst:
+#  print(i)
+
+# #  Find duplicate elements Remove duplicates (keep order)
+# c=[1,2,1,3,1,4]
+# d=[]
+# for i in c:
+#     if i not in d:
+#         d.append(i)
+# print(d)
+  
+#  Separate even and odd numbers Find second largest number
+# lst=[1,2,3,4,5,6,7,8]
+# even=[]
+# odd=[]
+# for i in lst:
+#     if i%2==0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+# print(even)
+# print(odd)
+# # Find second largest number
+# lst = [10, 50, 20, 40]
+# l=s=lst[0]
+# for i in lst:
+#     if i>l:
+#         l=i
+#     elif i>s and i!=l:
+#         s=i
+# print(s)
+# for i in range(1, 10):
+#     if i == 2:
+#         break
+#     print(i)
+
